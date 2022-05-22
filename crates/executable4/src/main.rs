a66988112043fe4e508c241e216045aa438c9413
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2022-05-22 09:41:46 -0700

    simplified (mac, example 4)

--- crates/executable4/src/main.rs
+++ crates/executable4/src/main.rs
@@ -4,12 +4,24 @@ extern crate mac4;
 
 use mac4::make_lookup_by_str_funct;
 
+#[derive(Debug,Copy,Clone)]
+enum Index {
+    Bruce,
+    Zane,
+    Klo,
+}
+
 // expand
 // $ cargo +nightly run -- -Zunpretty=expanded
 
 fn main() {
+
     make_lookup_by_str_funct!(
-        const xyzzy: [&str] = ["bruce", "zane", "klo"];
+        const xyzzy: (&str, Index) = [
+            ("bruce", Index::Bruce),
+            ("zane", Index::Zane),
+            ("klo", Index::Klo)
+        ];
     );
 
     for s in ["bruce", "klo", "zane", "other"] {
--- crates/mac4/src/lib.rs
+++ crates/mac4/src/lib.rs
@@ -4,7 +4,11 @@ use proc_macro::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::parse::{Parse, ParseStream, Result};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, ExprArray, Ident, Token, TypeSlice, Visibility};
+use syn::{
+    parse_macro_input,
+    Token,
+    Visibility, Ident, ExprArray, TypeTuple,
+};
 
 /// Parses the following syntax, which is based on dtolnay's work mimicking
 /// the `lazy_static` crate:
@@ -19,12 +23,30 @@ use syn::{parse_macro_input, ExprArray, Ident, Token, TypeSlice, Visibility};
 /// For example:
 ///
 ///     make_lookup_by_str_func! {
-///         pub const func_name: (&str, result_type) = $EXPR;
+///         pub const func_name: (&str, usize) = [
+///             ("bruce", 5),
+///             ("macnaughton", 11),
+///         ];
+///     }
+///
+/// generates:
+///
+///     const func_name_ITEMS: [(&str, usize); 2] = [
+///         ("bruce", 5),
+///         ("macnaughton", 11),
+///     ];
+///     pub fn func_name(s: &str) -> Option<usize> {
+///         for i in 0..func_name_ITEMS.len() {
+///             if s == func_name_ITEMS[i].0 {
+///                 return Some(func_name_ITEMS[i].1);
+///             }
+///         }
+///         None
 ///     }
 struct LookupThingByStr {
     visibility: Visibility,
     name: Ident,
-    type_slice: TypeSlice,
+    type_tuple: TypeTuple,
     init: ExprArray,
 }
 
@@ -34,14 +56,14 @@ impl Parse for LookupThingByStr {
         input.parse::<Token![const]>()?;
         let name: Ident = input.parse()?;
         input.parse::<Token![:]>()?;
-        let type_slice: TypeSlice = input.parse()?;
+        let type_tuple: TypeTuple = input.parse()?;
         input.parse::<Token![=]>()?;
         let init: ExprArray = input.parse()?;
         input.parse::<Token![;]>()?;
         Ok(LookupThingByStr {
             visibility,
             name,
-            type_slice,
+            type_tuple,
             init,
         })
     }
@@ -52,29 +74,17 @@ pub fn make_lookup_by_str_funct(input: TokenStream) -> TokenStream {
     let LookupThingByStr {
         visibility,
         name,
-        type_slice,
+        type_tuple,
         init,
     } = parse_macro_input!(input as LookupThingByStr);
 
-    println!("vis: {:?}", visibility);
-    println!("name: {:?}, to_string(): {}", name, name.to_string());
-    println!("type: {:?}", type_slice);
-    println!("type_slice: {:?}", type_slice.elem);
-    println!("init expr: {:?} init len: {}", init, init.elems.len());
-
-
-    // Assert that the static type implements Sync. If not, user sees an error
-    // message like the following. We span this assertion with the field type's
-    // line/column so that the error message appears in the correct place.
-    //
-    //     error[E0277]: the trait bound `*const (): std::marker::Sync` is not satisfied
-    //       --> src/main.rs:10:21
-    //        |
-    //     10 |     const PTR: *const [()] = [&()];
-    //        |                ^^^^^^^^^^^ `*const [()]` cannot be shared between threads safely
-    //let _assert_sync = quote_spanned! {ty.span()=>
-    //    struct _AssertSync where #ty: std::marker::Sync;
-    //};
+    //println!("vis: {:?}", visibility);
+    //println!("name: {:?}, to_string(): {}", name, name.to_string());
+    //println!("type_tuple (n = {}): {:?}", type_tuple.elems.len(), type_tuple);
+    //println!("type_tuple.elems[0]: {:?}", type_tuple.elems[0]);
+    //if let Type::Reference(tr) = &type_tuple.elems[0] {
+    //    //println!("type_ref: {:?}", tr.elem);
+    //}
 
     // Check for Sized. Not vital to check here, but the error message is less
     // confusing this way than if they get a Sized error in one of our
@@ -83,15 +93,30 @@ pub fn make_lookup_by_str_funct(input: TokenStream) -> TokenStream {
     //     error[E0277]: the trait bound `str: std::marker::Sized` is not satisfied
     //       --> src/main.rs:10:19
     //        |
-    //     10 |     const A: [str] = [""];
-    //        |               ^^^ `str` does not have a constant size known at compile-time
-    //let _assert_sized = quote_spanned! {ty.span()=>
-    //struct _AssertSized where #ty: std::marker::Sized;
-    //};
+    //     10 |     pub const fn_name: (str, Thing) = [("", Thing::One)];
+    //        |                         ^^^^^^^^^^ doesn't have a size known at compile-time
+    quote_spanned! (
+        type_tuple.elems[0].span()=> struct _x where type_tuple.elems[0]: std::marker::Sized
+    );
+
+    // Check the first element of the tuple for PartialEq. Not vital to check here,
+    // but the error message is less confusing this way than if they get a PartialEq
+    // error in one of our implementation details where it uses `==`.
+    //
+    // error[E0369]: binary operation `==` cannot be applied to type `Thing`
+    //   --> src/main.rs:10:19
+    //    |
+    // 10 |     const A: (&str, Thing) = [("a", Thing::One), ("b", Thing::Two)];
+    //    |
+    let _assert_partial_eq = quote_spanned! {type_tuple.elems[0].span()=>
+        struct _AssertParialEq where #type_tuple.elems[0]: std::cmd::PartialEq;
+    };
 
     let items_name = format!("{}_ITEMS", name);
     let items_name = Ident::new(&items_name, name.span());
-    let items_type = type_slice.elem;
+    let items_type = &type_tuple;
+    let lookup_type = &type_tuple.elems[0];
+    let return_type = &type_tuple.elems[1];
     let items_len = init.elems.len();
 
     let expanded = quote! {
@@ -99,10 +124,10 @@ pub fn make_lookup_by_str_funct(input: TokenStream) -> TokenStream {
         #[allow(non_upper_case_globals)]
         const #items_name: [#items_type; #items_len] = #init;
 
-        #visibility fn #name(s: &str) -> Option<usize> {
+        #visibility fn #name(s: #lookup_type) -> Option<#return_type> {
             for i in 0..#items_name.len() {
-                if s == #items_name[i] {
-                    return Some(i);
+                if s == #items_name[i].0 {
+                    return Some(#items_name[i].1);
                 }
             }
             None
